d6ee41c39095d6f636d16682670b2698a73012a7
rajy <kiddo.rajy@gmail.com>
2023-06-08 12:48:39 -0700

    Everything can be imported

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    a = 10
+    b = 5
 
-a = 10
-b = 5
-
-sum_result = add(a, b)
-diff_result = sub(a, b)
-prod_result = mul(a, b)
-quot_result = div(a, b)
+    sum_result = add(a, b)
+    diff_result = sub(a, b)
+    prod_result = mul(a, b)
+    quot_result = div(a, b)
 
 print("{} + {} = {}".format(a, b, sum_result))
 print("{} - {} = {}".format(a, b, diff_result))
--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 import sys
-args = sys.argv[1:]
-num_args = len(args)
-if num_args == 0:
-    print("0 arguments.")
-else:
-    plural = "" if num_args == 1 else "s"
-    print("{} argument{}:".format(num_args, plural))
-    for i, arg in enumerate(args, start=1):
-	    print("{}: {}".format(i, arg))
+if __name__ == "__main__":
+    args = sys.argv[1:] 
+    num_args = len(args)
+    if num_args == 0:
+        print("0 arguments.")
+    else:
+        plural = "" if num_args == 1 else "s"
+        print("{} argument{}:".format(num_args, plural))
+        for i, arg in enumerate(args, start=1):
+            print("{}: {}".format(i, arg))
