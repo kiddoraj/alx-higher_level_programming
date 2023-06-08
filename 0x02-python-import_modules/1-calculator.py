833f357b1a18531e2b0a614a17c94125f935cbe3
rajy <kiddo.rajy@gmail.com>
2023-06-08 11:21:03 -0700

    How to make a script dynamic

---
+++ 0x02-python-import_modules/1-calculator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+
+a = 10
+b = 5
+
+sum_result = add(a, b)
+diff_result = sub(a, b)
+prod_result = mul(a, b)
+quot_result = div(a, b)
+
+print("{} + {} = {}".format(a, b, sum_result))
+print("{} - {} = {}".format(a, b, diff_result))
+print("{} * {} = {}".format(a, b, prod_result))
+print("{} / {} = {}".format(a, b, quot_result))
