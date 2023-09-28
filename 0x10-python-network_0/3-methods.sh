44b24c1bf2ec35de8bf55b2c2e5d23b9bab8f900
rajy <kiddo.rajy@gmail.com>
2023-09-28 13:13:34 -0700

    Debugg

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 # This script sends an HTTP OPTIONS request to a URL and displays the accepted HTTP methods
-
-# Check if a URL is provided as an argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Send an HTTP OPTIONS request using curl and display the accepted methods
-curl -s -I -X OPTIONS "$1" | grep -i Allow | awk '{print $2}'
+curl -Is "$1" | grep "Allow:" | cut -d ":" -f 2 | cut -c 2- | rev | cut -c 2- | rev
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,18 +1,3 @@
 #!/bin/bash
 # This script sends a POST request to a URL with custom variables in the request body and displays the response body
-
-# Check if a URL is provided as the first argument
-if [ $# -ne 1 ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# URL to send the POST request to
-url="$1"
-
-# Define the variables to be sent in the request body
-email="test@gmail.com"
-subject="I will always be here for PLD"
-
-# Send a POST request with the custom variables in the request body using curl and display the response body
-curl -s -X POST -d "email=$email&subject=$subject" "$url"
+curl -s "$1" -X POST -d "email=test@gmail.com&subject=I will always be here for PLD"
